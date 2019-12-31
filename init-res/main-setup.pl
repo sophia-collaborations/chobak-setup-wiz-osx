f002409410b690663343771d48b3092afee25207
Sophia Shapira <sophiaphillyqueen@users.noreply.github.com>
2019-12-31 16:06:17 -0500

    Update main-setup.pl

--- init-res/main-setup.pl
+++ init-res/main-setup.pl
@@ -1,6 +1,10 @@
 #! /usr/bin/perl
 use strict;
 
+my $hme;
+
+$hme = $ENV{'HOME'};
+
 sub verifmul {
   my $lc_each;
   my $lc_loc;
@@ -14,6 +18,33 @@ sub verifmul {
   }
 }
 
-&verifmul('git');
+sub chadira {
+  my $lc_arg;
+  my $lc_ok;
+  my $lc_from;
+  foreach $lc_arg (@_) {
+    $lc_ok = chdir($lc_arg);
+    if ( $lc_ok < 0.5 )
+    {
+      $lc_from = `pwd`; chomp($lc_from);
+      die(
+        "\nCOULD NOT CHANGE DIRECTORY:" .
+        "\nFrom: " . $lc_from . " :" .
+        "\n  To: " . $lc_arg . " :" .
+      "\n\n");
+    }
+  }
+}
+
+
+&verifmul('git','curl','perl');
+
+&chadira($hme);
+system('mkdir bin 2> /dev/null');
+system('mkdir chobak-packs 2> /dev/null');
+system('mkdir chobak-packs/usrcmd 2> /dev/null');
+&chadira('chobak-packs/usrcmd');
+
+system('pwd');
 
 
