d15460fc091b43d712154f4a66f94715ab3d34a2
Sophia Shapira <sophiaphillyqueen@users.noreply.github.com>
2019-12-31 15:33:42 -0500

    Update firstoff.sh

--- init-res/firstoff.sh
+++ init-res/firstoff.sh
@@ -4,9 +4,17 @@
   mkdir ~/.chobak-setup-wiz 2> /dev/null
   cd ~/.chobak-setup-wiz && (
     curl https://raw.githubusercontent.com/sophia-collaborations/chobak-setup-wiz-osx/master/init-res/main-setup.pl -o main-setup.pl
-    perl main-setup.pl | die "
-    FAILED ATTEMPT
-    "
+    perl main-setup.pl | (
+      (
+        echo
+        echo FAILED ATTEMPT
+        echo
+      ) 2> /dev/null
+      exit 5
+    )
   )
+  echo
+  echo
+  echo COMPLETE
   echo && echo
 )
